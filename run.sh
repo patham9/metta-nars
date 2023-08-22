bd2212c62981025425d77cdbe0bbcbc5b0b7eaf9
Patrick Hammer <patham9@gmail.com>
2023-08-22 21:48:11 +0200

    Update: working goal derivation

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-                                    #tests.metta
-cat utils.metta nal.metta nars.metta tests2.metta > RUN.metta && metta RUN.metta
+cat utils.metta nal.metta nars.metta tests0.metta > RUN.metta && metta RUN.metta > OUTPUT_tests0.metta
+cat utils.metta nal.metta nars.metta tests1.metta > RUN.metta && metta RUN.metta > OUTPUT_tests1.metta
+cat utils.metta nal.metta nars.metta tests2.metta > RUN.metta && metta RUN.metta > OUTPUT_tests2.metta
+cat utils.metta nal.metta nars.metta tests3.metta > RUN.metta && metta RUN.metta > OUTPUT_tests3.metta
+
