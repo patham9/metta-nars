37d5ea65c9984c19f9c19b92c93449d3019f5043
Patrick Hammer <patham9@gmail.com>
2023-08-23 09:01:21 +0200

    Update: new directory structure

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
-cat utils.metta logic.metta memory.metta declarative.metta temporal.metta procedural.metta tests0.metta > RUN.metta && metta RUN.metta > OUTPUT_tests0.txt
-cat utils.metta logic.metta memory.metta declarative.metta temporal.metta procedural.metta tests1.metta > RUN.metta && metta RUN.metta > OUTPUT_tests1.txt
-cat utils.metta logic.metta memory.metta declarative.metta temporal.metta procedural.metta tests2.metta > RUN.metta && metta RUN.metta > OUTPUT_tests2.txt
-cat utils.metta logic.metta memory.metta declarative.metta temporal.metta procedural.metta tests3.metta > RUN.metta && metta RUN.metta > OUTPUT_tests3.txt
+cd src
+sh glue.sh
+cd ..
+cat ./src/NARS.metta ./tests/tests0.metta > RUN.metta && metta RUN.metta > OUTPUT_tests0.txt
+cat ./src/NARS.metta ./tests/tests1.metta > RUN.metta && metta RUN.metta > OUTPUT_tests1.txt
+cat ./src/NARS.metta ./tests/tests2.metta > RUN.metta && metta RUN.metta > OUTPUT_tests2.txt
+cat ./src/NARS.metta ./tests/tests3.metta > RUN.metta && metta RUN.metta > OUTPUT_tests3.txt
+rm RUN.metta
---
+++ src/glue.sh
@@ -0,0 +1,1 @@
+cat utils.metta logic.metta memory.metta declarative.metta temporal.metta procedural.metta > NARS.metta
