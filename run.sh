81df187fbf0e94ca281e6ceec2900dad68c91305
Patrick Hammer <patham9@gmail.com>
2023-08-23 08:34:55 +0200

    Update: proper modularization

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
-cat utils.metta nal.metta nars.metta tests0.metta > RUN.metta && metta RUN.metta > OUTPUT_tests0.metta
-cat utils.metta nal.metta nars.metta tests1.metta > RUN.metta && metta RUN.metta > OUTPUT_tests1.metta
-cat utils.metta nal.metta nars.metta tests2.metta > RUN.metta && metta RUN.metta > OUTPUT_tests2.metta
-cat utils.metta nal.metta nars.metta tests3.metta > RUN.metta && metta RUN.metta > OUTPUT_tests3.metta
-
+cat utils.metta logic.metta memory.metta declarative.metta temporal.metta procedural.metta tests0.metta > RUN.metta && metta RUN.metta > OUTPUT_tests0.txt
+cat utils.metta logic.metta memory.metta declarative.metta temporal.metta procedural.metta tests1.metta > RUN.metta && metta RUN.metta > OUTPUT_tests1.txt
+cat utils.metta logic.metta memory.metta declarative.metta temporal.metta procedural.metta tests2.metta > RUN.metta && metta RUN.metta > OUTPUT_tests2.txt
+cat utils.metta logic.metta memory.metta declarative.metta temporal.metta procedural.metta tests3.metta > RUN.metta && metta RUN.metta > OUTPUT_tests3.txt
