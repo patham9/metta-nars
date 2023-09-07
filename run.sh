994327cffb064c3172a0a5abe7a4ad3aa65aa335
Patrick Hammer <patham9@gmail.com>
2023-09-07 19:55:26 +0200

    Update: more convenient run script

---
+++ run.sh
@@ -0,0 +1,1 @@
+sh build.sh && cat ./src/NARS.metta $1 > RUN.metta && metta RUN.metta
--- tests.sh
+++ tests.sh
@@ -1,6 +1,4 @@
-cd src
 sh build.sh
-cd ..
 cat ./src/NARS.metta ./tests/tests0.metta > RUN.metta && metta RUN.metta > OUTPUT_tests0.txt
 cat ./src/NARS.metta ./tests/tests1.metta > RUN.metta && metta RUN.metta > OUTPUT_tests1.txt
 cat ./src/NARS.metta ./tests/tests2.metta > RUN.metta && metta RUN.metta > OUTPUT_tests2.txt
