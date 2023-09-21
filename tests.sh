2aa60d96bdfec97994d24e69624134edd47a3579
Patrick Hammer <patham9@gmail.com>
2023-09-21 07:33:25 +0200

    Update: additional test which demands declarative reasoning and decision making

--- tests.sh
+++ tests.sh
@@ -3,4 +3,6 @@ cat ./src/NARS.metta ./tests/tests0.metta > RUN.metta && metta RUN.metta > OUTPU
 cat ./src/NARS.metta ./tests/tests1.metta > RUN.metta && metta RUN.metta > OUTPUT_tests1.txt
 cat ./src/NARS.metta ./tests/tests2.metta > RUN.metta && metta RUN.metta > OUTPUT_tests2.txt
 cat ./src/NARS.metta ./tests/tests3.metta > RUN.metta && metta RUN.metta > OUTPUT_tests3.txt
+cat ./src/NARS.metta ./tests/tests4.metta > RUN.metta && metta RUN.metta > OUTPUT_tests4.txt
+cat ./src/NARS.metta ./tests/tests5.metta > RUN.metta && metta RUN.metta > OUTPUT_tests5.txt
 rm RUN.metta
