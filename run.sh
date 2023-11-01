44ac87bc3ce25ee1cd2740872b9cf35178fbb670
Patrick Hammer <patham9@gmail.com>
2023-11-01 18:19:42 +0100

    Update: Temporal pathway anticipation fixed

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 sh build.sh && cat ./src/NARS.metta $1 > RUN.metta
 cd ..
-time sh runmetta.sh ./metta-nars/RUN.metta
+#time sh runmetta.sh ./metta-nars/RUN.metta
 time sh runscheme.sh ./metta-nars/RUN.metta
