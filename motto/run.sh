0eef5d62457897b56672bc01e98f197806c04721
Patrick Hammer <patham9@gmail.com>
2024-01-31 23:33:11 +0100

    Update: MeTTa-Motto example for natural language input to MeTTa-NARS accelerated via MeTTa-Morph

--- motto/run.sh
+++ motto/run.sh
@@ -1,5 +1,6 @@
 echo "Welcome to MeTTa-Motto + MeTTa-NARS + MeTTa-Morph, please make sure to export OPENAI_API_KEY=..."
 cd metta-motto || (git clone https://github.com/trueagi-io/metta-motto && cd metta-motto)
-metta ./../nars_reason.metta 2>&1 | grep "^(" | sed 's/^/!/' > INPUT.metta
+cat ./../nars_reason.metta > RUN.metta
+metta RUN.metta 2>&1 | grep "^(" | sed 's/^/!/' > INPUT.metta
 cd ../../
 cat ./motto/metta-motto/INPUT.metta | sh run_scheme_repl_compiled.sh
