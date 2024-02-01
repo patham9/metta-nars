1fa0bff933d01605b9a764cddefec8268a948a66
Patrick Hammer <patham9@gmail.com>
2024-02-01 15:15:17 +0100

    Update: intermediate commit

--- motto/run.sh
+++ motto/run.sh
@@ -1,6 +1,12 @@
 echo "Welcome to MeTTa-Motto + MeTTa-NARS + MeTTa-Morph, please make sure to export OPENAI_API_KEY=..."
+cd ..
+sh build.sh
+cd motto
 cd metta-motto || (git clone https://github.com/trueagi-io/metta-motto && cd metta-motto)
-cat ./../nars_reason.metta > RUN.metta
-metta RUN.metta 2>&1 | grep "^(" | sed 's/^/!/' > INPUT.metta
-cd ../../
-cat ./motto/metta-motto/INPUT.metta | sh run_scheme_repl_compiled.sh
+cp -r motto ../../../extend/
+cd ../../../extend/
+cp ../metta-nars/src/NARS.metta NARS.metta
+cp ../metta-nars/motto/nars_reason.metta nars_reason.metta
+cp ../metta-nars/motto/nars_reason.msa nars_reason.msa
+metta example1.metta
+#metta nars_reason.metta
